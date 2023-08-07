ab374219f616582cd2a8c93aa5724ad1343e91b0
Matteo Foccoli <matteo.foccoli@gmail.com>
2023-08-07 08:32:46 +0200

    Lesson 22: Implemented Order Track Command Handler

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseListenerImpl.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Slf4j
+@Validated
+public class RestaurantApprovalResponseListenerImpl implements
+        RestaurantApprovalResponseListener {
+    @Override
+    public void orderApproved(RestaurantApprovalResponse response) {
+
+    }
+
+    @Override
+    public void orderRejected(RestaurantApprovalResponse response) {
+
+    }
+}
