28f7465e0c650dc599f1f855ef58d0458743ce2a
Matteo Foccoli <matteo.foccoli@gmail.com>
2023-08-25 08:27:51 +0200

    Lesson 31: Adding web controller (fixes also wrong import from Jakarta!)

---
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -0,0 +1,45 @@
+package com.food.ordering.system.order.service.application.rest;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/orders", produces = "application/vnd.api.v1+json")
+public class OrderController {
+
+    private final OrderApplicationService orderApplicationService;
+
+    public OrderController(OrderApplicationService orderApplicationService) {
+        this.orderApplicationService = orderApplicationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(
+            @RequestBody CreateOrderCommand command) {
+        log.info("Creating order for customer {} at restaurant {}",
+                command.getCustomerId(),
+                command.getRestaurantId());
+        CreateOrderResponse response = orderApplicationService.createOrder(command);
+        log.info("Order created with tracking id {}", response.getOrderTrackingId());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{trackingId}")
+    public ResponseEntity<TrackOrderResponse> getOrderByTrackingId(
+            @RequestParam UUID trackingId) {
+        log.info("Getting order by tracking id {}", trackingId);
+        TrackOrderResponse response = orderApplicationService.trackOrder(
+                TrackOrderQuery.builder().orderTrackingId(trackingId).build());
+        log.info("Returning order with tracking id {}", response.getOrderTrackingId());
+        return ResponseEntity.ok(response);
+    }
+}
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -2,7 +2,7 @@ package com.food.ordering.system.order.service.domain.dto.create;
 
 
 import com.food.ordering.system.domain.valueobject.OrderStatus;
-import jakarta.validation.constraints.NotNull;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
