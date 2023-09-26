7618ac90a1650e38d4195a4813a923883ad2412b
Matteo Foccoli <matteo.foccoli@gmail.com>
2023-09-26 08:38:00 +0200

    Lesson 42. Container module: Adding Order database schema file

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseListenerImpl.java
@@ -1,7 +1,7 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
-import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseListener;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -10,7 +10,7 @@ import org.springframework.validation.annotation.Validated;
 @Slf4j
 @Validated
 public class RestaurantApprovalResponseListenerImpl implements
-        RestaurantApprovalResponseListener {
+        RestaurantApprovalResponseMessageListener {
     @Override
     public void orderApproved(RestaurantApprovalResponse response) {
 
